8b6ed880d664f56b62c270bf7e997af35b3aecf6
Perry Hunchak <phunchak@gmail.com>
2020-05-21 23:16:05 -0600

    init

--- EasingIntoReact/src/index.js
+++ EasingIntoReact/src/index.js
@@ -1,17 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Tweet from "./Tweet";
 
-const data = {
-  message: "A tweet about something cool",
-  gravatar: "5c3dd2d257ff0e14dbd2583485dbd44b",
-  author: {
-    handle: "dceddia",
-    name: "Dave Ceddia",
-  },
-  likes: 17,
-  retweets: 3,
-  timestamp: "2019-07-10 21:24:37",
-};
 
-ReactDOM.render(<Tweet tweet={data} />, document.querySelector("#root"));
+ReactDOM.render(, document.querySelector("#root"));
